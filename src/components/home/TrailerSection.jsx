5b4f9fa25d07316e9e3097b21ee97073d7b7a807
K-taiyi <korm.taiyi289@gmail.com>
2025-01-20 14:42:24 +0700

    add trailer

--- src/components/home/TrailerSection.jsx
+++ src/components/home/TrailerSection.jsx
@@ -1,6 +1,22 @@
 import backgroundImage from '../../assets/1.jpg';
+import { useState,useEffect } from 'react';
 
 const TrailerSection = () => {
+
+  const [value,setValue]=useState([])
+  async function callData(){
+    try {
+      const resp = await fetch("https://api.themoviedb.org/3/discover/tv?api_key=aacdbe83dedab8fc913bd72adf3fdbad");
+      const json = await resp.json();
+      setValue(json.results)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(()=>{
+    callData();
+  },[])
+
   return (
     <div className="h-[380px] bg-cover bg-no-repeat bg-center" style={{ backgroundImage: `url(${backgroundImage})`}}>
       <div className="h-full pl-4 sm:pl-[40px] md:pl-[70px] content-center py-8 w-full bg-gray-600 bg-opacity-30">
@@ -8,17 +24,19 @@ const TrailerSection = () => {
         <div className="overflow-x-scroll">
           <div className="flex gap-4">  
             { 
-              <div>
-                <iframe 
-                  width="300px" 
-                  height="160px" 
-                  src="https://www.youtube.com/embed/TFHCew8DnC0?list=RDql9VWZ3KfQg&index=4"> 
-                </iframe>
-                <div className="w-full text-center min-h-[50px] mt-3">
-                  <h1 className="text-white text-xl">faed{}</h1>
-                  <p className=" text-white text-sm">sf{}</p>
+              value.map((v)=>(
+                <div key={v.id}>
+                  <iframe 
+                    width="300px" 
+                    height="160px" 
+                    src={`${v.backdrop_path}`}> 
+                  </iframe>
+                  <div className="w-full text-center min-h-[50px] mt-3">
+                    <h1 className="text-white text-xl">{v.name}</h1>
+                    <p className=" text-white text-sm">{}</p>
+                  </div>
                 </div>
-              </div>
+              ))
             }
           </div>
         </div>
